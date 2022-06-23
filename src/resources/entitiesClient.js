5b51fa3e3c1fefcac6ee0003d5578fd29b04c4fe
Iago Luiz <iago.barbosa@nouvenn.com>
2022-06-23 10:30:42 -0300

    :tada:

--- src/resources/entitiesClient.js
+++ src/resources/entitiesClient.js
@@ -3,8 +3,7 @@ import client from './config';
 const path = '/presents';
 
 const Client = {    
-    async getGifts(data) {
-        console.log(data)
+    async getGifts(data) {        
         try {
             const response = await client.post(
                 path,
